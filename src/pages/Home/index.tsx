8f69c7891315df4dd0c415dbe42521938f41ce01
Patrick Morais <ppternunes@gmail.com>
2021-12-28 16:33:35 -0300

    add google login button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 import { SwitchTheme } from '../../components/SwitchTheme';
 import * as S from './style';
 
 export default function Home() {
+  const client_id = String(process.env.CLIENT_ID);
+
+  const successResponseGoogle = () => {};
+
+  const failureResponseGoogle = () => {};
+
   return (
     <S.HomeSection>
       <h1>Photobase</h1>
       <p>Save your photos beautifully in Firebase</p>
+      <GoogleLogin
+        clientId={client_id}
+        buttonText="Login with Google"
+        onSuccess={successResponseGoogle}
+        onFailure={failureResponseGoogle}
+      />
       <SwitchTheme />
     </S.HomeSection>
   );
