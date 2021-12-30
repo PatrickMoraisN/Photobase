56e17b4215f76c76466b6eb76a2bdb81868e7bfc
Patrick Morais <ppternunes@gmail.com>
2021-12-30 18:02:56 -0300

    fix user email - GetAllPhotosService

--- src/services/GetAllPhotosService.ts
+++ src/services/GetAllPhotosService.ts
@@ -1,12 +1,14 @@
 import { ref, listAll, getDownloadURL } from 'firebase/storage';
 import { PhotoProps } from '../types/Photo';
 import { storage } from '../firebase/firebase';
+import { useGoogleAuthInfo } from '../hooks/useGoogleAuthInfo';
 
 class GetAllPhotosService {
   async execute() {
     const list: PhotoProps[] = [];
+    const { email: userEmail } = useGoogleAuthInfo();
 
-    const imagesFolder = ref(storage, 'images');
+    const imagesFolder = ref(storage, `images/${userEmail}`);
     const photosList = await listAll(imagesFolder);
 
     for (const i in photosList.items) {
