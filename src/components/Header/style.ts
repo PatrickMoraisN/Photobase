6c8307ba70c95398dbb67e9e17a5a1d2a740afa6
Patrick Morais <ppternunes@gmail.com>
2021-12-30 12:17:31 -0300

    fix header style

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -4,7 +4,7 @@ export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 18px 30px;
+  padding: 18px 60px;
   background: ${(props) => props.theme.colors.background_header};
 `;
 
