ab5ce9dbf9b506d3c80d93c3b4dac77d0230dd01
Patrick Morais <ppternunes@gmail.com>
2021-12-30 23:19:43 -0300

    create InsertPhotoService

---
+++ src/services/InsertPhotoService.tsx
@@ -0,0 +1,28 @@
+import { getDownloadURL, ref, uploadBytes } from '@firebase/storage';
+import { v4 as uuid } from 'uuid';
+import { storage } from '../firebase/firebase';
+import { PhotoProps } from '../types/Photo';
+
+class InsertPhotoService {
+  async execute(file: File) {
+    const localStorageUser = localStorage.getItem('@photobase-user');
+    if (
+      ['image/jpeg', 'image/jpg', 'image/png'].includes(file.type) &&
+      localStorageUser
+    ) {
+      const { email } = JSON.parse(localStorageUser);
+      const namePhotoId = uuid();
+
+      const newFile = ref(storage, `images/${email}/${namePhotoId}`);
+
+      const upload = await uploadBytes(newFile, file);
+
+      const photoUrl = await getDownloadURL(upload.ref);
+
+      return { name: upload.ref.name, url: photoUrl } as PhotoProps;
+    }
+    return new Error('Upload Fail');
+  }
+}
+
+export { InsertPhotoService };
