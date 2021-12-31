096190ebba499bfe4517b2fd1c0112cf0fee69f5
Patrick Morais <ppternunes@gmail.com>
2021-12-30 23:10:52 -0300

    add UploadPhoto

--- src/components/UploadPhoto/index.tsx
+++ src/components/UploadPhoto/index.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 
 export function UploadPhoto() {
-  return <div>opa</div>;
+  const [isUploading, setIsUploading] = React.useState<boolean>();
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if (file && file.size > 0) {
+      setIsUploading(true);
+
+      setIsUploading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input type="file" name="image" />
+      {isUploading ? (
+        <button type="submit">Uploading...</button>
+      ) : (
+        <button type="submit">Add Photo</button>
+      )}
+    </form>
+  );
 }
